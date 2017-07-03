b7752d3ada26a5e9b4a379267b087a10c51a9c17
James Hollins <james.hollins@bashton.com>
2017-07-03 21:14:01 +0100

    Added disable api termination variable

--- instances.tf
+++ instances.tf
@@ -1,13 +1,14 @@
 # Domain Controller Instances
 resource "aws_instance" "domain_controller" {
-  count                  = "${var.dc_count}"
-  ami                    = "${data.aws_ami.windows.id}"
-  instance_type          = "${var.instance_type}"
-  key_name               = "${var.key_name}"
-  subnet_id              = "${element(var.private_subnets, count.index )}"
-  iam_instance_profile   = "${module.iam_instance_profile_domain_controllers.profile_id}"
-  vpc_security_group_ids = ["${var.security_groups}", "${aws_security_group.sg_domain_controllers.id}"]
-  user_data              = "${count.index == 0 ? "<powershell>${data.template_file.primary_domain_controller.rendered}</powershell><persist>true</persist>" : "<powershell>${data.template_file.domain_controllers.rendered}</powershell><persist>true</persist>" }"
+  count                   = "${var.dc_count}"
+  ami                     = "${data.aws_ami.windows.id}"
+  instance_type           = "${var.instance_type}"
+  key_name                = "${var.key_name}"
+  subnet_id               = "${element(var.private_subnets, count.index )}"
+  iam_instance_profile    = "${module.iam_instance_profile_domain_controllers.profile_id}"
+  vpc_security_group_ids  = ["${var.security_groups}", "${aws_security_group.sg_domain_controllers.id}"]
+  user_data               = "${count.index == 0 ? "<powershell>${data.template_file.primary_domain_controller.rendered}</powershell><persist>true</persist>" : "<powershell>${data.template_file.domain_controllers.rendered}</powershell><persist>true</persist>" }"
+  disable_api_termination = "${var.disable_api_termination}"
 
   root_block_device {
     volume_type = "${var.instance_ebs_type}"
